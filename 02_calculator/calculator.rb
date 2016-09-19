ae15e2f4724470359cc38df9fd48fc486bdec303
shimbiro <shimbirosharon@gmail.com>
2016-09-19 22:43:07 +0300

    calculations

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,40 @@
+#def add
+	def add(a,b)
+	a+b
+
+end
+def subtract(f,t)
+	f-t 
+end
+def sum(v=[])
+	v.inject(0) {|sum,x| sum + x.to_i }
+end
+def multiply (a,b)
+	a * b
+end
+def multiplies (y,z)
+	y * z
+end
+def power(m,n)
+	m ** n
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
